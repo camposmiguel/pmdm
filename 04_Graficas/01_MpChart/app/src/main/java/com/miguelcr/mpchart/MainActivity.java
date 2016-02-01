4ac431b6d4a42aa91847aa55397fa0ef884a7dac
Miguel Campos <camposmiguel@gmail.com>
2016-02-01 13:14:11 +0100

    Multimedia

---
+++ 04_Graficas/01_MpChart/app/src/main/java/com/miguelcr/mpchart/MainActivity.java
@@ -0,0 +1,57 @@
+package com.miguelcr.mpchart;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    BarChart mChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //http://code.tutsplus.com/tutorials/add-charts-to-your-android-app-using-mpandroidchart--cms-23335
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        BarChart chart = new BarChart(this);
+        setContentView(chart);
+
+        BarData data = new BarData(labels, dataset);
+        chart.setData(data);
+
+        chart.setDescription("# of times Alice called Bob");
+
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        chart.animateY(5000);
+    }
+}
---
+++ 05_Multimedia/02_SoundPool/app/src/main/java/com/miguelcr/soundpool/MainActivity.java
@@ -0,0 +1,43 @@
+package com.miguelcr.soundpool;
+
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    SoundPool soundPool;
+    int sonidoExplosion;
+    Button btnPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPlay = (Button)findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(this);
+
+
+        // Player properties
+        AudioAttributes aa = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(10)
+                .setAudioAttributes(aa)
+                .build();
+
+        // Load sound of guitar
+        sonidoExplosion = soundPool.load(this,R.raw.powerup,1);
+    }
+
+    @Override
+    public void onClick(View v) {
+        soundPool.play(sonidoExplosion,1,1,0,0,1);
+    }
+}
